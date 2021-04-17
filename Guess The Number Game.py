94aca717a0c14519a1e049a432da580ab1e00627
Tejas Gupta <82724333+tejasgupta27@users.noreply.github.com>
2021-04-17 23:25:54 +0530

    Update Guess The Number Game.py

--- Guess The Number Game.py
+++ Guess The Number Game.py
@@ -1,9 +1,9 @@
-    n = 1024
-    i = 9
+    n = 1024 #number already given to computer
+    i = 9 #number of chances
     print("Welcome to Guess the Number Game!\nNo. of guesses left: 10")
     x='Y'
     while i>=0 and (x=='Y' or x=='y'):
-
+        #when all chances are over
         if i == 0:
             print("Your chances are over! :\\ \n Game Over!")
             x = input("Play again? (Y/N)")
@@ -13,22 +13,25 @@
                 print("Thanks for Playing!")
             else:
                 print("Invalid Input!")
-
-        a = int(input("Guess the number:\n"))
+        
+        a = int(input("Guess the number:\n")) #a = number guessed by end user
         print("No. of guesses left:", i)
 
+        #when guessed number is more than given number
         if a>n:
             print("I guess you shoult try a smaller number...")
             i-=1
             print("No. of guesses left:", i)
             continue
 
+        #when guessed number is less that given number
         elif a<n:
             print("Maybe you should try a larger number... ;)")
             i-=1
             print("No. of guesses left:", i)
             continue
 
+        #when user guesses the number in first attempt
         elif a==n and i==9:
             print("Amazing! You guessed it in the first try! You'r epic!!")
             print("Congrats, You Won!")
@@ -40,6 +43,7 @@
                 print("Thanks for Playing!")
                 break
 
+        #when user guesses the number, but not in first attempt
         elif a==n:
             print("You guessed it right without using all the 10 chances!!")
             print("Congrats, You Won!")
