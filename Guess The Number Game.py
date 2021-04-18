9f5123985030312bcedd723193527a8e10465c6e
Tejas Gupta <82724333+tejasgupta27@users.noreply.github.com>
2021-04-18 19:56:03 +0530

    Update Guess The Number Game.py

--- Guess The Number Game.py
+++ Guess The Number Game.py
@@ -1,56 +1,56 @@
-    n = 1024 #number already given to computer
-    i = 9 #number of chances
-    print("Welcome to Guess the Number Game!\nNo. of guesses left: 10")
-    x='Y'
-    while i>=0 and (x=='Y' or x=='y'):
-        #when all chances are over
-        if i == 0:
-            print("Your chances are over! :\\ \n Game Over!")
-            x = input("Play again? (Y/N)")
-            if x == "Y" or x == "y":
-                i=9
-            elif x == "N":
-                print("Thanks for Playing!")
-            else:
-                print("Invalid Input!")
-        
-        a = int(input("Guess the number:\n")) #a = number guessed by end user
+n = 1024 #number already given to computer
+i = 9 #number of chances
+print("Welcome to Guess the Number Game!\nNo. of guesses left: 10")
+x='Y'
+while i>=0 and (x=='Y' or x=='y'):
+    #when all chances are over
+    if i == 0:
+        print("Your chances are over! :\\ \n Game Over!")
+        x = input("Play again? (Y/N)")
+        if x == "Y" or x == "y":
+            i=9
+        elif x == "N":
+            print("Thanks for Playing!")
+        else:
+            print("Invalid Input!")
+    
+    a = int(input("Guess the number:\n")) #a = number guessed by end user
+    print("No. of guesses left:", i)
+
+     #when guessed number is more than given number
+    if a>n:
+        print("I guess you shoult try a smaller number...")
+        i-=1
         print("No. of guesses left:", i)
+        continue
 
-        #when guessed number is more than given number
-        if a>n:
-            print("I guess you shoult try a smaller number...")
-            i-=1
-            print("No. of guesses left:", i)
+     #when guessed number is less that given number
+    elif a<n:
+        print("Maybe you should try a larger number... ;)")
+        i-=1
+        print("No. of guesses left:", i)
+        continue
+
+     #when user guesses the number in first attempt
+    elif a==n and i==9:
+        print("Amazing! You guessed it in the first try! You'r epic!!")
+        print("Congrats, You Won!")
+        x = input("Play again? (Y/N)")
+        if x == "Y" or x == "y":
+            i = 9
             continue
+        elif x == "N" or x == "n":
+            print("Thanks for Playing!")
+            break
 
-        #when guessed number is less that given number
-        elif a<n:
-            print("Maybe you should try a larger number... ;)")
-            i-=1
-            print("No. of guesses left:", i)
+     #when user guesses the number, but not in first attempt
+    elif a==n:
+        print("You guessed it right without using all the 10 chances!!")
+        print("Congrats, You Won!")
+        x = input("Play again? (Y/N)")
+        if x == "Y" or x == "y":
+            i = 9
             continue
-
-        #when user guesses the number in first attempt
-        elif a==n and i==9:
-            print("Amazing! You guessed it in the first try! You'r epic!!")
-            print("Congrats, You Won!")
-            x = input("Play again? (Y/N)")
-            if x == "Y" or x == "y":
-                i = 9
-                continue
-            elif x == "N" or x == "n":
-                print("Thanks for Playing!")
-                break
-
-        #when user guesses the number, but not in first attempt
-        elif a==n:
-            print("You guessed it right without using all the 10 chances!!")
-            print("Congrats, You Won!")
-            x = input("Play again? (Y/N)")
-            if x == "Y" or x == "y":
-                i = 9
-                continue
-            elif x == "N" or x == "n":
-                print("Thanks for Playing!")
-                break
+        elif x == "N" or x == "n":
+            print("Thanks for Playing!")
+            break
